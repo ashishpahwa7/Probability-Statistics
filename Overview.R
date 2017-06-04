bdb9b1181f19c34798122ca6a95ba3e987c47154
Ashish Pahwa <ashishpahwa7@gmail.com>
2017-06-04 23:45:29 +0530

    Delete Overview.R

--- Overview.R
+++
@@ -1,7 +0,0 @@
-load(file = "depression.RData")
-head(depression)
-
-depression$Gender = replace(depression$Gender,depression$Gender==1,'Female'); 
-depression$Gender = replace(depression$Gender,depression$Gender==2,'Male'); 
-
-depression$Gender
